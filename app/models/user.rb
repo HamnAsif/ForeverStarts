b54410edde25d975fbc276bb30e2e2f2e6bdef8d
HamnAsif <hamnaasif665@gmail.com>
2025-07-03 01:51:54 -0700

    change user to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   belongs_to :authenticatable, polymorphic: true, optional: true
-  def couple?
-  authenticatable_type == "Couple"
-  end
 
-  def vendor?
-    authenticatable_type == "Vendor"
-  end
 end
